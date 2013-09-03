6f7029c02b4c01c3d4b18103d19934a867af619b
lu bing <w.t.start@gmail.com>
2013-09-03 09:19:39 +0800

    Initial Commit

---
+++ TestWebViewInteraction/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestWebViewInteraction
+//
+//  Created by sheldon on 13-9-3.
+//  Copyright (c) 2013年 sheldon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestWebViewInteraction/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestWebViewInteraction
+//
+//  Created by sheldon on 13-9-3.
+//  Copyright (c) 2013年 sheldon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
